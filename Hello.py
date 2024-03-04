c6dba15a4e55eecfcc1c675066c66d620d9bd7e0
cnetseo <ccasazza@redventures.com>
2024-03-04 15:18:12 -0500

    changes to homepage

---
+++ Hello.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+def main():
+    st.set_page_config(
+        page_title="Hello",
+        page_icon="👋",
+    )
+
+    st.write("# Welcome to RV SEO Apps 👋")
+
+    st.sidebar.success("Choose between the following apps.")
+
+    st.markdown(
+        """
+        This streamlit is a repository of data analytics and AI scripts developed by SEOs at RV. Choose from the following apps. 
+
+        Maintained by @ccasazza
+        
+    """
+    )
+
+if __name__ == "__main__":
+    main()
