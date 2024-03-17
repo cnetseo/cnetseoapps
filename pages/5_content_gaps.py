c8a639358203b9bc253437c86e37e5f92586b5df
cnetseo <ccasazza@redventures.com>
2024-03-17 15:26:05 -0400

    Update 5_content_gaps.py

--- pages/5_content_gaps.py
+++ pages/5_content_gaps.py
@@ -201,6 +201,7 @@ def content_gaps_module(urls,keyword, headers):
                                     - Content gaps are defined as key information, facts or concepts that are present in page 2 but absent in page 1
                                     - The main topic of the page is related to  {keyword} so use that to guide what you consider a content gap.
                                     - Use the domain names instead of "page 1" or "page 2" for example 
+                                    -Provide examples for each content gap you identify
                                 """
                         },
                         {
@@ -248,6 +249,7 @@ def content_gaps_module(urls,keyword, headers):
     class ContentGap(BaseModel):
         content_gap_name: str = Field(description="name of content gap between page i.e Content Gap 1, Content Gap 2")
         content_gap_description: str = Field(description="description of content gap between pages")
+        content_gap_example: str = Field(description="Examples of content gaps found")
 
     parser = SimpleJsonOutputParser(pydantic_object=ContentGap)
     
