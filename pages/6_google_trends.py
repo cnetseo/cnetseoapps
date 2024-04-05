7332387db1f0f12d98307f165793d54d441f504d
cnetseo <ccasazza@redventures.com>
2024-04-05 13:32:11 -0400

    Update 6_google_trends.py

--- pages/6_google_trends.py
+++ pages/6_google_trends.py
@@ -31,6 +31,7 @@ def fetch_google_trends_data(keywords, lookback_period):
             interest_data = json_response['interest_over_time']['timeline_data']
             #print(interest_data)
             row_data = {"Keyword": keyword}
+            print(row_data)
 
             for data_point in interest_data:
                 date_format = '%Y-%m'
@@ -40,6 +41,7 @@ def fetch_google_trends_data(keywords, lookback_period):
                     date = pd.to_datetime(data_point['date'])
                     month_year = date.strftime(date_format)
                     date_list = [month_year]
+                    
                 elif lookback_period == 'today 12-m':
                     value = data_point['values'][0]['extracted_value']
                     #print(value)
